8ad4bf1ee0bd3c232941d0f9f609957f43590eb0
kprashant49 <kprashant49@gmail.com>
2025-05-24 00:28:12 +0530

    Export mysql table contents to Excel file code added.

--- my_new_project/query_mysql_pd_dataframe.py
+++ my_new_project/query_mysql_pd_dataframe.py
@@ -13,10 +13,12 @@ database=config["database"]
 engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
 
 # Correct way to use `pd.read_sql()`
-query = "SELECT * FROM users a, orders b where a.id = b.user_id;"  # Example query
+# query = "SELECT * FROM users a, orders b where a.id = b.user_id;"  # Example query
+query = "SELECT * FROM pincode_master;"  # Example query
 df = pd.read_sql(query, con=engine)  # Pass both the query and the connection
 
 # Display DataFrame
 # print(df)
-counts = df.groupby('name').count()
-print(counts)
+# counts = df.groupby('name').count()
+# print(counts)
+df.to_excel(r"C:\Users\kpras\Desktop\Pincode_Master.xlsx", index=False)
