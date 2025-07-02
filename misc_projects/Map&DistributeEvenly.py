89a52c2b2dc65dd8b54a04b338e8a19bf0775908
kprashant49 <kprashant49@gmail.com>
2025-07-02 14:29:05 +0530

    Map&DistributeEvenly fixed variable names as per actual files

--- misc_projects/Map&DistributeEvenly.py
+++ misc_projects/Map&DistributeEvenly.py
@@ -5,8 +5,8 @@ import json
 
 # Importing current month's allocation
 filepath_input = r"C:\Users\kpras\Desktop\Test_data\Allocation.xlsx"
-df = pd.read_excel(filepath_input,sheet_name='Sheet1', engine = 'openpyxl')
-table_a = df[['AGREEMENTID', 'MAILINGZIPCODE']]
+allocation_new = pd.read_excel(filepath_input,sheet_name='Sheet1', engine = 'openpyxl')
+df_allocation_new = allocation_new[['AGREEMENTID', 'MAILINGZIPCODE']]
 
 with open('db_config.json') as f:
     config = json.load(f)
@@ -23,54 +23,54 @@ cursor = conn.cursor()
 cursor.execute("SELECT AGREEMENTID, AWS_CODE FROM allocation_retail where MAILINGZIPCODE  = '411021'")
 rows = cursor.fetchall()
 columns = [col[0] for col in cursor.description]
-df_allocation = pd.DataFrame(rows, columns=columns)
+df_allocation_old = pd.DataFrame(rows, columns=columns)
 
-table_a['AGREEMENTID'] = table_a['AGREEMENTID'].astype(str)
-df_allocation['AGREEMENTID'] = df_allocation['AGREEMENTID'].astype(str)
-merged_df1 = pd.merge(table_a, df_allocation, how='left', on='AGREEMENTID', indicator=True)
-merged_df2 = merged_df1[merged_df1['_merge'] == 'both']
-merged_df2['_merge'] = 'Assigned basis old allocation'
-merged_df2.rename(columns={'_merge': 'Allocation_Status'}, inplace=True)
+# Priority 1 mapping
+df_allocation_new['AGREEMENTID'] = df_allocation_new['AGREEMENTID'].astype(str)
+df_allocation_old['AGREEMENTID'] = df_allocation_old['AGREEMENTID'].astype(str)
+df_map_priority1 = pd.merge(df_allocation_new, df_allocation_old, how='left', on='AGREEMENTID', indicator=True)
+df_priority1 = df_map_priority1[df_map_priority1['_merge'] == 'both']
+df_priority1['_merge'] = 'Assigned basis old allocation'
+df_priority1.rename(columns={'_merge': 'Allocation_Status'}, inplace=True)
+
 # Create a counter column that increments per AWS_CODE
-merged_df2['FOS_mapping_count'] = merged_df1.groupby('AWS_CODE').cumcount() + 1
-merged_df2['MAILINGZIPCODE'] = merged_df2['MAILINGZIPCODE'].astype(str)
-merged_df2 = merged_df2.dropna()
+df_priority1['FOS_mapping_count'] = df_map_priority1.groupby('AWS_CODE').cumcount() + 1
+df_priority1['MAILINGZIPCODE'] = df_priority1['MAILINGZIPCODE'].astype(str)
+df_priority1 = df_priority1.dropna()
 
-exclusive_A = merged_df1[merged_df1['_merge'] == 'left_only']
-exclusive_A = exclusive_A[table_a.columns]
+df_non_map_priority1 = df_map_priority1[df_map_priority1['_merge'] == 'left_only']
+df_non_map_priority1 = df_non_map_priority1[df_allocation_new.columns]
 
-# Preparing for agent mapping
+# Loading data for Priority 2 mapping
 cursor = conn.cursor()
 cursor.execute("SELECT MAILINGZIPCODE, AWS_CODE, FOS_NAME FROM pai_emp_pincode_mapper")
 rows = cursor.fetchall()
 columns = [desc[0] for desc in cursor.description]
 cursor.close()
 conn.close()
-table_b = pd.DataFrame(rows, columns=columns)
+employee_pincode_mapper = pd.DataFrame(rows, columns=columns)
 
-# Convert ZIP codes to string and strip any leading/trailing spaces
-exclusive_A = exclusive_A[['AGREEMENTID', 'MAILINGZIPCODE']].copy()
-exclusive_A['MAILINGZIPCODE'] = exclusive_A['MAILINGZIPCODE'].astype(str).str.strip()
-table_b['MAILINGZIPCODE'] = table_b['MAILINGZIPCODE'].astype(str).str.strip()
+df_non_map_priority1 = df_non_map_priority1[['AGREEMENTID', 'MAILINGZIPCODE']].copy()
+df_non_map_priority1['MAILINGZIPCODE'] = df_non_map_priority1['MAILINGZIPCODE'].astype(str).str.strip()
+employee_pincode_mapper['MAILINGZIPCODE'] = employee_pincode_mapper['MAILINGZIPCODE'].astype(str).str.strip()
 
-# Pre-existing mapping counts (can be empty!)
+# Calculating FOS mapping counts after Priority 1 mapping and assigning it to a dictionary for multi-counters (can be empty!)
+# Using default dictionary for safe default to 0 if FOS not present
 # existing_counts = {'431': 30}
-merged_df2['AWS_CODE'].value_counts()
-existing_counts = merged_df2['AWS_CODE'].value_counts().to_dict()
-
-# Safe default to 0 if key not present
+df_priority1['AWS_CODE'].value_counts()
+existing_counts = df_priority1['AWS_CODE'].value_counts().to_dict()
 assignment_counter = defaultdict(int, existing_counts)
 result = []
 
-# Assign A rows to least-loaded B rows for each common_key
-for idx, row in exclusive_A.iterrows():
+# Assign AgreementID rows to least-loaded FOS rows for each Zipcode
+for idx, row in df_non_map_priority1.iterrows():
     key = row['MAILINGZIPCODE']
 
-    # Get all matching B rows for this key
-    b_group = table_b[table_b['MAILINGZIPCODE'] == key]['AWS_CODE'].tolist()
+    # Get all matching FOS rows for this Zipcode
+    fos_group = employee_pincode_mapper[employee_pincode_mapper['MAILINGZIPCODE'] == key]['AWS_CODE'].tolist()
 
-    if not b_group:
-        # No matching B for this A — skip or assign None
+    if not fos_group:
+        # No matching FOS for this Zipcode — skip or assign None
         result.append({
             'index': idx,
             'AGREEMENTID': row['AGREEMENTID'],
@@ -81,33 +81,32 @@ for idx, row in exclusive_A.iterrows():
         })
         continue
 
-    # Find least-loaded B based on current + existing
-    # min_b = min(b_group, key=lambda b: assignment_counter[b]) - Another method
+    # Find least-loaded FOS based on current + existing mapping
+    # min_fos = min(fos_group, key=lambda x: assignment_counter[x]) - Another method
     min_count = None
-    min_b = None
-    for b in b_group:
-        count = assignment_counter[b]
+    min_fos = None
+    for x in fos_group:
+        count = assignment_counter[x]
         if min_count is None or count < min_count:
             min_count = count
-            min_b = b
-    # Assign and update count
-    assignment_counter[min_b] += 1
+            min_fos = x
+    # Assign and update counter
+    assignment_counter[min_fos] += 1
 
     result.append({
         'index': idx,
         'AGREEMENTID': row['AGREEMENTID'],
         'MAILINGZIPCODE': key,
-        'AWS_CODE': min_b,
+        'AWS_CODE': min_fos,
         'Allocation_Status': 'Assigned by rule_engine',
-        'FOS_mapping_count': assignment_counter[min_b]
+        'FOS_mapping_count': assignment_counter[min_fos]
     })
 
-mapped_df2 = pd.DataFrame(result).set_index('index').sort_index().reset_index(drop=True)
-mapped_df3 = pd.concat([merged_df2, mapped_df2], ignore_index=True)
-mapped_df3['AGREEMENTID'] = mapped_df3['AGREEMENTID'].astype(str)
-table_a['AGREEMENTID'] = table_a['AGREEMENTID'].astype(str)
-mapped_df3['AGREEMENTID'] = pd.Categorical(mapped_df3['AGREEMENTID'], categories=table_a['AGREEMENTID'], ordered=True)
-df2_sorted = mapped_df3.sort_values('AGREEMENTID')
-# df2_sorted.drop('FOS_mapping_count', axis=1, inplace=True)
-df2_sorted.to_excel(r"C:\Users\kpras\Desktop\Auto_allocation.xlsx", index=False)
+df_priority2 = pd.DataFrame(result).set_index('index').sort_index().reset_index(drop=True)
+allocation_combined = pd.concat([df_priority1, df_priority2], ignore_index=True)
+allocation_combined['AGREEMENTID'] = allocation_combined['AGREEMENTID'].astype(str)
+allocation_combined['AGREEMENTID'] = pd.Categorical(allocation_combined['AGREEMENTID'], categories=df_allocation_new['AGREEMENTID'], ordered=True)
+Auto_allocation = allocation_combined.sort_values('AGREEMENTID')
+# Auto_allocation.drop('FOS_mapping_count', axis=1, inplace=True)
+Auto_allocation.to_excel(r"C:\Users\kpras\Desktop\Auto_allocation.xlsx", index=False)
 print("Exported the output to an excel file named 'Auto_allocation.xlsx'")
