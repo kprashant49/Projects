5f2e7eadb9ef0adc8d471a4845630c2caaf2adc1
Prashant Kumar <kprashant49@gmail.com>
2025-07-20 01:22:47 +0530

    RPS updated with an auto-move mode

--- Games/RPS.py
+++ Games/RPS.py
@@ -1,10 +1,20 @@
+from random import choice
+
+players = int(input("Please enter numer of players: "))
+while players not in [1,2]:
+	players = int(input("Please enter numer of players: "))
+
 player1 = input("Player 1, make your move: ")
 while player1.lower() not in ["rock","paper","scissors"]:
 	player1 = input("Player 1, make your move: ")
 
-player2 = input("Player 2, make your move: ")
-while player2.lower() not in ["rock","paper","scissors"]:
+if players == 2:
 	player2 = input("Player 2, make your move: ")
+	while player2.lower() not in ["rock","paper","scissors"]:
+		player2 = input("Player 2, make your move: ")
+else:
+	player2 = choice(["rock", "paper", "scissors"])
+	print(f"Player 2 moved {player2}")
 
 if player1 == player2:
 	print("It's a tie!")
