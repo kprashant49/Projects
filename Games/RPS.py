9acdb0f0488e4d0c4485a30fe80eded6b59077e1
Prashant Kumar <kprashant49@gmail.com>
2025-07-21 02:13:48 +0530

    RPS updated with custom match tournament

--- Games/RPS.py
+++ Games/RPS.py
@@ -4,10 +4,18 @@ players = int(input("Please enter numer of players: "))
 while players not in [1,2]:
 	players = int(input("Please enter numer of players: "))
 
+Matches = int(input("Please enter number of matches: "))
+Winner = round((Matches/2),0)
+
 player1_count = 0
 player2_count = 0
 
-for games in range(3):
+# for games in range(Matches):
+while player1_count < Winner and player2_count < Winner:
+	if players == 1:
+		print(f"Player 1: {player1_count} Computer: {player2_count}")
+	else:
+		print(f"Player 1: {player1_count} Player 2: {player2_count}")
 
 	player1 = input("Player 1, make your move: ")
 	while player1.lower() not in ["rock","paper","scissors"]:
@@ -39,11 +47,17 @@ for games in range(3):
 		print("Player 2 wins!")
 		player2_count += 1
 
-print(f"Player 1 won {player1_count} games.")
-print(f"Player 2 won {player2_count} games.")
-if player1_count > player1_count:
+
+if players == 1:
+	print(f"Player 1: {player1_count} Computer: {player2_count}")
+else:
+	print(f"Player 1: {player1_count} Player 2: {player2_count}")
+
+if player1_count > player2_count:
 	print("Player 1 is the winner!")
-elif player1_count < player2_count:
+elif player1_count < player2_count and players == 1:
+	print("Computer is the winner!")
+elif player1_count < player2_count and players == 2:
 	print("Player 2 is the winner!")
 else:
 	print("It's a tie!")
