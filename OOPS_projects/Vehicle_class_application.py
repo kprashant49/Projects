bd8d865431611762876fd88d5725b919626bc47d
Prashant Kumar <kprashant49@gmail.com>
2025-07-23 01:38:13 +0530

    Working on OOPS project

--- OOPS_projects/Vehicle_class_application.py
+++ OOPS_projects/Vehicle_class_application.py
@@ -1,20 +1,31 @@
 from Vehicle_class import Vehicle, Truck, Motorcycle
 car1 = Vehicle("Jeep","Toyota","Black")
 truck1 = Truck("Pick-up","Chevy","White")
+
 print(car1.vehicle_make)
 print(car1.vehicle_body)
 print(car1.vehicle_colour)
 print(car1.Manufactured_in)
-car1.drive()
+
 print(truck1.vehicle_make)
 print(truck1.vehicle_body)
 print(truck1.vehicle_colour)
 print(truck1.Manufactured_in)
-truck1.drive()
+
 print(truck1.get_vehicle_count())
 motorcycle1 = Motorcycle("250cc","Honda","Black")
 print(motorcycle1.vehicle_make)
 print(motorcycle1.vehicle_body)
 print(motorcycle1.vehicle_colour)
 print(motorcycle1.Manufactured_in)
-motorcycle1.drive()
+
+# Polymorphism
+for veh in [truck1, car1, motorcycle1]:
+    veh.drive()
+
+def perform_tasks(vehicle_object):
+    vehicle_object.drive()
+
+perform_tasks(truck1)
+perform_tasks(car1)
+perform_tasks(motorcycle1)
