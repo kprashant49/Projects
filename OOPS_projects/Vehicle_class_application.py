93a54b61667b1c307e619031eb905c9bbc1ddf01
Prashant Kumar <kprashant49@gmail.com>
2025-07-23 01:02:24 +0530

    Working on OOPS project

---
+++ OOPS_projects/Vehicle_class_application.py
@@ -0,0 +1,7 @@
+from Vehicle_class import Vehicle
+car1 = Vehicle("Jeep","Toyota","Black")
+print(car1.vehicle_make)
+print(car1.vehicle_body)
+print(car1.vehicle_colour)
+print(car1.Manufactured_in)
+car1.drive()
