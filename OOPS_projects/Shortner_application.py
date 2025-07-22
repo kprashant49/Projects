f2b13f6826bb1524788c174553ee7232c2f3a18f
Prashant Kumar <kprashant49@gmail.com>
2025-07-23 02:10:42 +0530

    Working on OOPS project

--- OOPS_projects/Shortner_application.py
+++ OOPS_projects/Shortner_application.py
@@ -1,5 +1,14 @@
-from Shortner import DictionaryShortner
+from Shortner import *
 
 
 myshortner = DictionaryShortner({1:"mike",2:"tom",3:"john",4:"kim"})
+myshortner.print_original_items()
+myshortner.print_shortened_items()
+
+myshortner = ListAndCharShortner([1,2,3,4,5,6,7,8,9,10])
+myshortner.print_original_items()
+myshortner.print_shortened_items()
+
+myshortner = ListAndCharShortner("Sliced")
+myshortner.print_original_items()
 myshortner.print_shortened_items()
