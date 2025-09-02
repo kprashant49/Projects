05cc0d047b9e2372dc6f9dcd5552749b19204851
Prashant Kumar <kprashant49@gmail.com>
2025-09-02 19:16:13 +0530

    New project added status_mailer added for sending email basis table data

--- Status_mailer/status_mailer.py
+++ Status_mailer/status_mailer.py
@@ -6,26 +6,46 @@ import requests
 from Snowflake_connection import get_snowflake_connection, load_config
 
 query = """
-SELECT 'ICICI' AS "Bank's Name",
-       MAX(TO_DATE(lot_dt, 'DD-MON-YY')) AS "Latest Transaction Date"
-FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_ICICI
-
-UNION ALL
-
-SELECT 'CITI' AS "Bank's Name",
-       MAX(TO_DATE(lot_dt, 'DD-MON-YY')) AS "Latest Transaction Date"
+SELECT 
+    'Pepper' AS "Entity",
+    'Citibank' AS "Bank",
+     TO_VARCHAR(MAX(TO_DATE(TO_TIMESTAMP(SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 1) || ' ' ||SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 2),'DD/MM/YYYY HH24:MI:SS'))),'DD/MM/YYYY') AS "Latest Transaction Date",
+     'Rs.'|| (SELECT BALANCE_INR FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_CITI WHERE GLOBAL_RANK = (SELECT max(GLOBAL_RANK) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_CITI)) AS "Closing Balance",
+     TO_VARCHAR(MAX(TO_DATE(lot_dt, 'DD-MON-YY')),'DD/MM/YYYY') AS "Last Data Load Date",
+     (SELECT COUNT(NEW_CATEGORY) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_CITI WHERE NEW_CATEGORY = 'Other') AS "Unmapped Transaction(s)"
 FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_CITI
 
 UNION ALL
 
-SELECT 'HDFC' AS "Bank's Name",
-       MAX(TO_DATE(lot_dt, 'DD-MON-YY')) AS "Latest Transaction Date"
+SELECT 
+    'Pepper' AS "Entity",
+    'HDFC Bank' AS "Bank",
+    TO_VARCHAR(MAX(TO_DATE(TO_TIMESTAMP(SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 1) || ' ' ||SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 2),'DD/MM/YYYY HH24:MI:SS'))),'DD/MM/YYYY') AS "Latest Transaction Date",
+    'Rs.'|| (SELECT BALANCE_INR FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_HDFC WHERE GLOBAL_RANK = (SELECT max(GLOBAL_RANK) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_HDFC)) AS "Closing Balance",
+    TO_VARCHAR(MAX(TO_DATE(lot_dt, 'DD-MON-YY')),'DD/MM/YYYY') AS "Last Data Load Date",
+    (SELECT COUNT(NEW_CATEGORY) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_HDFC WHERE NEW_CATEGORY = 'Other') AS "Unmapped Transaction(s)"
 FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_HDFC
 
 UNION ALL
 
-SELECT 'Saraswa' AS "Bank's Name",
-       MAX(TO_DATE(lot_dt, 'DD-MON-YY')) AS "Latest Transaction Date"
+SELECT 
+    'Rieom' AS "Entity",
+    'ICICI Bank' AS "Bank",
+     TO_VARCHAR(MAX(TO_DATE(TO_TIMESTAMP(SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 1) || ' ' ||SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 2),'DD/MM/YYYY HH24:MI:SS'))),'DD/MM/YYYY') AS "Latest Transaction Date",
+     'Rs.'|| (SELECT BALANCE_INR FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_ICICI WHERE GLOBAL_RANK = (SELECT max(GLOBAL_RANK) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_ICICI)) AS "Closing Balance",
+     TO_VARCHAR(MAX(TO_DATE(lot_dt, 'DD-MON-YY')),'DD/MM/YYYY') AS "Last Data Load Date",
+     (SELECT COUNT(NEW_CATEGORY) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_ICICI WHERE NEW_CATEGORY = 'Other') AS "Unmapped Transaction(s)"
+FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_ICICI
+
+UNION ALL
+
+SELECT 
+    'Rieom' AS "Entity",
+    'Saraswat Bank' AS "Bank",
+    TO_VARCHAR(MAX(TO_DATE(TO_TIMESTAMP(SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 1) || ' ' ||SPLIT_PART(TRANSACTION_POSTED_DATE, ' ', 2),'DD/MM/YYYY HH24:MI:SS'))),'DD/MM/YYYY') AS "Latest Transaction Date",
+    'Rs.'|| (SELECT BALANCE_INR FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_SARAS WHERE GLOBAL_RANK = (SELECT max(GLOBAL_RANK) FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_SARAS)) AS "Closing Balance",
+    TO_VARCHAR(MAX(TO_DATE(lot_dt, 'DD-MON-YY')),'DD/MM/YYYY') AS "Last Data Load Date",
+    (SELECT COUNT(NEW_CATEGORY)FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_SARAS WHERE NEW_CATEGORY = 'Other') AS "Unmapped Transaction(s)"
 FROM GROUP_SHARE_INDIA.FINANCE.MARTS_CASH_FLOW_SARAS
 """
 # ---------------- Logging ----------------
@@ -44,7 +64,6 @@ def query_to_df(query, config):
         return df
     finally:
         conn.close()
-
 
 # ---------------- Outlook Mailer ----------------
 def load_outlook_config(path="config.ini"):
@@ -115,8 +134,14 @@ def status_mailer():
         df_html = df.to_html(index=False, border=1, justify="center")
 
         # Send Outlook email
-        subject = "India Cashflow Latest Transaction Report"
-        body_html = f"<h3>Latest Transaction Dates per Bank</h3>{df_html}"
+        subject = "India Cashbook Latest Transaction Report"
+        # body_html = f"<h3>Latest Transaction Dates per Bank</h3>{df_html}"
+        body_html = f"""
+                <p>Dear All,</p>
+                <p>Please find below the latest transaction dates for each bank.</p>
+                {df_html}
+                <p>Regards,<br>D&A Team</p>
+                """
         send_outlook_mail(subject, body_html, outlook_config)
 
     except Exception as e:
