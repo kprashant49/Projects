45efd23dff55df357e41f3b4b6abd85c5afe3c92
Prashant Kumar <kprashant49@gmail.com>
2025-10-02 03:37:52 +0530

    SQL practice notes appended

--- Database_interaction_projects/query_mysql_pd_dataframe.py
+++ Database_interaction_projects/query_mysql_pd_dataframe.py
@@ -14,11 +14,16 @@ engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
 
 # Correct way to use `pd.read_sql()`
 # query = "SELECT * FROM users a, orders b where a.id = b.user_id;"  # Example query
-query = "SELECT * FROM pincode_master;"  # Example query
+# query = "SELECT * FROM pincode_master;"  # Example query
+# query = "Select reverse(upper('Why does my cat looks at me with such hatred?'))"
+# query = "Select replace(title,' ','->') as title from books"
+# query = "Select author_fname as forward, reverse(author_fname) as backwards from books"
+query = "Select upper(concat(author_fname,' ',author_lname)) as FULLNAME from books"
+
 df = pd.read_sql(query, con=engine)  # Pass both the query and the connection
 
 # Display DataFrame
-# print(df)
+print(df)
 # counts = df.groupby('name').count()
 # print(counts)
-df.to_excel(r"C:\Users\kpras\Desktop\Pincode_Master.xlsx", index=False)
+# df.to_excel(r"C:\Users\kpras\Desktop\Pincode_Master.xlsx", index=False)
