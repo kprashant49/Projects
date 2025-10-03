c6bf081e3455e2220e28931936b27f8ff981baa2
Prashant Kumar <kprashant49@gmail.com>
2025-10-04 02:23:38 +0530

    SQL practice notes appended

--- misc_projects/MySQL_relational_practice_notes.sql
+++ misc_projects/MySQL_relational_practice_notes.sql
@@ -8,7 +8,25 @@ Select b.*, a.* from customers a
 right join orders b
 on a.id = b.customer_id;
 
+SELECT * FROM orders
+JOIN customers ON customers.id = orders.customer_id;
 
+SELECT first_name, last_name, sum(amount) as total FROM orders
+JOIN customers ON customers.id = orders.customer_id
+group by first_name, last_name
+order by total desc;
 
+Select first_name, last_name, ifnull(sum(amount),0) as money_spent from customers a 
+left join orders b
+on a.id = b.customer_id
+group by first_name, last_name;
 
+select first_name, title, grade from students join papers on students.id = papers.student_id order by grade desc;
 
+select first_name, ifnull(title,'MISSING') as title, ifnull(grade, 0) as grade from students left join papers on students.id = papers.student_id;
+
+select first_name, round(ifnull(avg(grade),0),2) as average,
+case when round(ifnull(avg(grade),0),2) >= 75 then "Pass"
+else "Fail"
+end as result
+from students left join papers on students.id = papers.student_id group by first_name order by average desc;
