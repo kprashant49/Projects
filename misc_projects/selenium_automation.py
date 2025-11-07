8467a00ecca7f88f3beadf4abfa0f2b67de28517
Prashant Kumar <kprashant49@gmail.com>
2025-11-08 01:03:48 +0530

    Started web scraping using Selenium

--- misc_projects/selenium_automation.py
+++ misc_projects/selenium_automation.py
@@ -9,12 +9,25 @@ time.sleep(5)
 
 # Navigate to login page
 driver.get("https://practicetestautomation.com/practice-test-login/")
-time.sleep(10)
+time.sleep(5)
 
-# Login
-driver.find_element(By.ID, "username").send_keys("student")
-driver.find_element(By.ID, "password").send_keys("Password123")
-driver.find_element(By.ID, "submit").click()
+# Load the locators
+username_locator = driver.find_element(By.ID, "username")
+password_locator = driver.find_element(By.ID, "password")
+submit_form_locator = driver.find_element(By.ID, "submit")
+
+# Submit the values
+username_locator.send_keys("student")
+password_locator.send_keys("Password123")
+submit_form_locator.click()
 
 # Wait for login and redirect
-time.sleep(10)
+time.sleep(5)
+
+# Verify successful login
+text_locator = driver.find_element(By.TAG_NAME, "h1")
+log_out_button = driver.find_element(By.LINK_TEXT, "Log out")
+
+# Log Out
+log_out_button.click()
+time.sleep(5)
