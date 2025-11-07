79eecee9dd48767a629f14360172f005cd31e1ad
Prashant Kumar <kprashant49@gmail.com>
2025-11-08 01:22:04 +0530

    Started web scraping using Selenium

--- misc_projects/selenium_automation.py
+++ misc_projects/selenium_automation.py
@@ -5,16 +5,18 @@ import time
 
 # Setup
 driver = webdriver.Chrome()  # Or use Firefox, Edge, etc.
-time.sleep(5)
+time.sleep(2)
 
 # Navigate to login page
 driver.get("https://practicetestautomation.com/practice-test-login/")
-time.sleep(5)
+time.sleep(2)
 
 # Load the locators
 username_locator = driver.find_element(By.ID, "username")
-password_locator = driver.find_element(By.ID, "password")
-submit_form_locator = driver.find_element(By.ID, "submit")
+# password_locator = driver.find_element(By.ID, "password")
+password_locator = driver.find_element(By.NAME, "password")
+# submit_form_locator = driver.find_element(By.ID, "submit")
+submit_form_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
 
 # Submit the values
 username_locator.send_keys("student")
@@ -22,12 +24,19 @@ password_locator.send_keys("Password123")
 submit_form_locator.click()
 
 # Wait for login and redirect
-time.sleep(5)
+time.sleep(2)
 
-# Verify successful login
+# Verify successful login ("https://practicetestautomation.com/logged-in-successfully/")
+actual_url = driver.current_url
+assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
 text_locator = driver.find_element(By.TAG_NAME, "h1")
-log_out_button = driver.find_element(By.LINK_TEXT, "Log out")
+actual_text = text_locator.text
+
+assert actual_text == "Logged In Successfully"
 
 # Log Out
+log_out_button = driver.find_element(By.LINK_TEXT, "Log out")
+assert log_out_button.is_displayed()
 log_out_button.click()
-time.sleep(5)
+time.sleep(2)
